b4b31d7c3c759afe3779db082c6f43a00b3a809d
Akihiro Nakamura <30426163+akihiro-n@users.noreply.github.com>
2020-12-05 04:49:49 +0900

    レイアウト調整

--- app/src/main/java/com/example/practicejetpackcompose/ui/NewArticleListItem.kt
+++ app/src/main/java/com/example/practicejetpackcompose/ui/NewArticleListItem.kt
@@ -3,11 +3,15 @@ package com.example.practicejetpackcompose.ui
 import com.example.practicejetpackcompose.model.ArticleDpo
 
 sealed class NewArticleListItem {
+
     object Progress : NewArticleListItem()
+
+    object Space : NewArticleListItem()
 
     data class Article(val article: ArticleDpo) : NewArticleListItem()
 
     data class Error(val throwable: Throwable) : NewArticleListItem() {
+
         val message: String
             get() = throwable.localizedMessage.orEmpty()
     }
